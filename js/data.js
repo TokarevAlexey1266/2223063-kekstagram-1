3e581df41fd775c6d0129c4cf65a46d659e17cab
TokarevAlexey1266 <106770342+TokarevAlexey1266@users.noreply.github.com>
2022-12-19 19:46:44 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,53 +3,54 @@ import { getRandomNumber } from './util.js';
 const PHOTO_COUNT = 25;
 
 const NAMES = [
-  'Алексей',
-  'Виктор',
-  'Василиса',
-  'Василий',
-  'Валентин',
-  'Анна',
-  'Алена'
+'Алексей',
+'Виктор',
+'Василиса',
+'Василий',
+'Валентин',
+'Анна',
+'Алена'
 ];
 
-const MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Пойдет и ладно.',
-  'Нормально',
-  'Чудесно все'
+const MESSAGES = [
+'Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Пойдет и ладно.',
+'Нормально',
+'Чудесно все'
 ];
 
 const allPhotoInfo = [];
 
 function createPhotoDescriptions () {
-  
+
   for (let i = 0; i < PHOTO_COUNT; i++) {
     allPhotoInfo[i] = {
       id: i + 1,
       url: `photos/${i}.jpg`,
       description: `Описание ${index + 1}`,
       likes: getRandomNumber(15, 200),
-      comments: createComments(getRandomNumber(1,17))
+      comments: getComments(getRandomNumber(1, 17))
     };
   }
   return allPhotoInfo;
 }
 
-function createComments() {
-  
-  const comments = [];
-  for (let i = 0; i < PHOTO_COUNT; i++){
-    comments[i] = {
-      id: i + 1,
-      avatar: `img/avatar${getRandomNumber(1, 6)}.svg`,
-      message: MESSAGE[getRandomNumber(0, MESSAGES.length - 1))],
-      name: NAMES[getRandomNumber(0, NAMES.length - 1))],
+function getComments() {
+  const commentsData = [];
+
+  for (let index = 0; index < PHOTO_COUNT; index++) {
+    commentsData[index] = {
+      id: index + 1,
+      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+      message: MESSAGES[getRandomNumber(0, MESSAGES.length - 1)],
+      name: NAMES[getRandomNumber(0, NAMES.length - 1)],
     };
   }
 
-  return comments;
+  return commentsData;
 }
 
 createPhotoDescriptions();
-export {NAMES, MESSAGE, createPhotoDescriptions, createComments, allPhotoInfo};
+
+export { allPhotoInfo ,NAMES, MESSAGES, DESCRIPTIONS, createPhotoDescriptions, getComments };
